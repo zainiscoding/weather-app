723ed148af853cead3ecbdee2e8d8605d7872132
Zain <zainiscoding@gmail.com>
2020-11-09 17:39:44 +0900

    Fixed a bug that caused getWeather() to call before the coordinates were set

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import getWeather from './components/GetWeatherContainer';
 // import getWeather from './components/GetWeatherContainer';
 
 const App = () => {
   let [weather, setWeather] = useState({});
-  let [img, setImg] = useState('');
   let [coordinates, setCoordinates] = useState({});
 
-  useEffect(() => {
-    function getLocation() {
-      setCoordinates((prevState) => {
-        navigator.geolocation.getCurrentPosition((position) => {
-          prevState.latitude = position.coords.latitude;
-        });
-        navigator.geolocation.getCurrentPosition((position) => {
-          prevState.longitude = position.coords.longitude;
-        });
-        return prevState;
+  function getLocation() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      return setCoordinates({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
       });
-    }
+    });
+  }
+
+  const initialRender = useRef(true);
+
+  useEffect(() => {
     getLocation();
   }, []);
 
   useEffect(() => {
-    setTimeout(function () {
-      console.log(coordinates);
+    if (initialRender.current) {
+      initialRender.current = false;
+    } else {
       getWeather();
-    }, 100);
-  }, []);
+    }
+  }, [coordinates]);
 
   async function getWeather() {
     try {
