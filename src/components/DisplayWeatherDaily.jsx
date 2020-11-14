5ba6d538e6a4a1354a4f2aeffcfe89d46cc6f538
Zain <zainiscoding@gmail.com>
2020-11-14 22:25:54 +0900

    Moved daily forecast heading out of carousel

--- src/components/DisplayWeatherDaily.jsx
+++ src/components/DisplayWeatherDaily.jsx
@@ -2,43 +2,46 @@ import { add } from 'date-fns';
 
 const DisplayWeatherDaily = (props) => {
   return (
-    <div id='display-weather-wrapper__daily-weather'>
+    <>
       <h3 id='daily-weather__title'>8-day Forecast</h3>
-      <div id='daily-weather__carousel'>
-        <div id='carousel__daily-weather'>
-          {props.weather.daily.map((day, index) => {
-            return (
-              <div className='display-weather-wrapper__day' key={index}>
-                <p>
-                  {add(props.currentDay, {
-                    days: props.weather.daily.indexOf(day),
-                  }).getMonth() + 1}
-                  /
-                  {add(props.currentDay, {
-                    days: props.weather.daily.indexOf(day),
-                  }).getDate() + 1}
-                </p>
-                <img
-                  src={
-                    'http://openweathermap.org/img/wn/' +
-                    day.weather[0].icon +
-                    '.png'
-                  }
-                  alt='Day weather icon'
-                />
-                <div id='daily-weather__details-box'>
-                  <div className='details-box__detail'>
-                    <p className='detail__text'>
-                      {Math.round(day.temp.max)}° / {Math.round(day.temp.min)}°
-                    </p>
+      <div id='display-weather-wrapper__daily-weather'>
+        <div id='daily-weather__carousel'>
+          <div id='carousel__daily-weather'>
+            {props.weather.daily.map((day, index) => {
+              return (
+                <div className='display-weather-wrapper__day' key={index}>
+                  <p>
+                    {add(props.currentDay, {
+                      days: props.weather.daily.indexOf(day),
+                    }).getMonth() + 1}
+                    /
+                    {add(props.currentDay, {
+                      days: props.weather.daily.indexOf(day),
+                    }).getDate() + 1}
+                  </p>
+                  <img
+                    src={
+                      'http://openweathermap.org/img/wn/' +
+                      day.weather[0].icon +
+                      '.png'
+                    }
+                    alt='Day weather icon'
+                  />
+                  <div id='daily-weather__details-box'>
+                    <div className='details-box__detail'>
+                      <p className='detail__text'>
+                        {Math.round(day.temp.max)}° / {Math.round(day.temp.min)}
+                        °
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
